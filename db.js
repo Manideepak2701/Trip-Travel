dcde0890175c8f6c3da41153b981cfc226603cbb
J Sai Manideepak <110416334+Manideepak2701@users.noreply.github.com>
2023-10-09 19:29:24 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+var mongoDBURL = 'mongodb+srv://deepak:kinna2004@cluster0.chjgd43.mongodb.net/travel'
+
+mongoose.connect(mongoDBURL , {useUnifiedTopology:true , useNewUrlParser:true})
+
+var dbconnect = mongoose.connection
+
+dbconnect.on('error' , ()=>{
+    console.log(`Mongo DB Connection Failed`);
+})
+
+dbconnect.on('connected' , ()=>{
+    console.log(`Mongo DB Connection Successfull`);
+})
+
+module.exports = mongoose
